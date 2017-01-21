0f95d39646fc35643ae42d3e7ff4223bd7472a9d
DazEdword <darkouterheaven@gmail.com>
2017-01-21 15:04:18 +0000

    Adding stub classes for creatures.

---
+++ Unity Project/Abyss/Assets/Scripts/Creatures/Brains/Wanderer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wanderer : Brain
+{
+    /*
+    public float timerToMove = 0;
+
+    public int chanceToStop = 4;
+
+    public override void GetInput()
+    {
+        //base.GetInput();
+
+        if (timerToMove <= 0)
+            MoveToRandomPosition();
+        else
+        {
+            timerToMove -= Creature.RefreshFrequency;
+            if (timerToMove < 0)
+                timerToMove = 0;
+        }
+
+    }
+
+    void MoveToRandomPosition()
+    {
+        if (Random.Range(1, 10) < chanceToStop)
+        {
+            Creature.Locomotor.Stop();
+        }
+        else
+        {
+
+            Vector3 nuPos = transform.position;
+            nuPos.x += (.5f - Random.value) * 5.0f;
+            nuPos.z += (.5f - Random.value) * 3.0f;
+            Creature.Locomotor.TargetPosition = nuPos;
+        }
+
+        timerToMove = 1;
+    }
+    */
+}
---
+++ Unity Project/Abyss/Assets/Scripts/Creatures/Creature.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creature : MonoBehaviour {
+
+    public static Creature Player;
+
+    public enum CREATURES {Fish, Jellyfish, Anemone, END};
+
+    //[HideInInspector]
+    //public Graphic Graphic;
+
+    [HideInInspector]
+    public Locomotor Locomotor;
+
+    [HideInInspector]
+    public Brain Brain;
+
+    [HideInInspector]
+    public Animator Animator;
+
+    [HideInInspector]
+    public SpriteRenderer Sprite;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
