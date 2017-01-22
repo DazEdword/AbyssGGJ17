a91243f246c1a7e99e7c4c5b28c5212069a59eef
eduardolm <edward.lm87@gmail.com>
2017-01-22 00:28:05 +0000

    few things

---
+++ Unity Project/Abyss/Assets/Scripts/Littlefish.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Littlefish : MonoBehaviour
+{
+    public float Frequency = 2;
+    public float Power = 0.25f;
+    public float Time = 0.25f;
+    public iTween.EaseType EaseType = iTween.EaseType.easeOutExpo;
+
+    void Start()
+    {
+        InvokeRepeating("Move", Frequency, Frequency);
+    }
+
+    void Move()
+    {
+        iTween.MoveBy(gameObject, iTween.Hash("z", Power, "time", Time, "easetype", EaseType));
+    }
+}
